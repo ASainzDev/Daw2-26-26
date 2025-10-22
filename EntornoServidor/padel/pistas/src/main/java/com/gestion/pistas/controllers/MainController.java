2a9979b8f07fb0e21eaba28c0765e303eba5677a
SainzA <alejandrosainzsainz@outlook.es>
2025-10-22 13:01:39 +0200

    Miercoles mañana casa

--- EntornoServidor/padel/pistas/src/main/java/com/gestion/pistas/controllers/MainController.java
+++ EntornoServidor/padel/pistas/src/main/java/com/gestion/pistas/controllers/MainController.java
@@ -1,12 +1,19 @@
 package com.gestion.pistas.controllers;
 
 
+import com.gestion.pistas.model.Pista;
 import com.gestion.pistas.services.AlmacenamientoPistas;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,21 +21,34 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 
 
-@CrossOrigin(origins = "http://localhost:8080")
-@RestController
+@CrossOrigin
+@Controller
 @RequestMapping("/")
 
 public class MainController {
     
 
     @Autowired
-    private AlmacenamientoPistas listado;
+    private AlmacenamientoPistas almacenamiento;
+
+    @GetMapping("/")
+    public String mostrarPagina(Model model) {
+        model.addAttribute("pistas", almacenamiento.obtenerTodasLasPistas());
+        return "index";
+    }
 
     @PostMapping("/addPista")
-    public void postMethodName(@RequestParam String nombrePista, String horasDisponibles) {
+    public String agregarPista(
+            @RequestParam String nombrePista,
+            @RequestParam String horasDisponibles) {
         
-        listado.addPista(nombrePista, horasDisponibles);
-        
+        almacenamiento.addPista(nombrePista, horasDisponibles);
+        return "redirect:/";
     }
-    
+
+    @GetMapping("/api/pistas")
+    @ResponseBody
+    public List<Pista> listarPistas() {
+        return almacenamiento.obtenerTodasLasPistas();
+    }
 }
--- EntornoServidor/padel/pistas/src/main/java/com/gestion/pistas/services/AlmacenamientoPistas.java
+++ EntornoServidor/padel/pistas/src/main/java/com/gestion/pistas/services/AlmacenamientoPistas.java
@@ -1,20 +1,27 @@
 package com.gestion.pistas.services;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
 
 import com.gestion.pistas.model.Pista;
 
+@Service
 public class AlmacenamientoPistas {
 
     
     Pista pista;
 
-    ArrayList<Pista> pistas = new ArrayList<Pista>();
+    private ArrayList<Pista> pistas = new ArrayList<Pista>();
 
     public void addPista(String nombre, String id){
         pista = new Pista(nombre, id);
         pistas.add(pista);
+    }
+
+    public List<Pista> obtenerTodasLasPistas() {
+        return new ArrayList<>(pistas);
     }
     
 }
